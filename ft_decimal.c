4a3264f2d62aab66fcc48840a25378af49fa85f6
Rustamjon Yusupov <ryusupov@2-D-5.42heilbronn.de>
2024-03-22 13:02:54 +0100

    fixed some bugs

---
+++ ft_decimal.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_decimal.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryusupov <ryusupov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 17:51:35 by ryusupov          #+#    #+#             */
+/*   Updated: 2024/03/21 20:31:58 by ryusupov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_decimal(size_t i, int res)
+{
+	int	recursive_result;
+
+	if (i > 9)
+	{
+		recursive_result = ft_decimal(i / 10, res);
+		if (recursive_result == -1)
+			return (-1);
+		res = recursive_result;
+	}
+	if (ft_putchar(i % 10 + '0') == -1)
+		return (-1);
+	res++;
+	return (res);
+}
