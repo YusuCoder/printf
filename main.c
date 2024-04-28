579a8739630cd0ea04c76878dd465dda5704c034
~Rustam____ <93910006+YusuCoder@users.noreply.github.com>
2024-04-28 23:48:41 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 22:51:48 by mac               #+#    #+#             */
+/*   Updated: 2024/04/28 23:27:14 by mac              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int main(void)
+{
+	char str[] = "Student!";
+	int num = 42;
+	ft_printf(COLOR_GREEN"The result is: "RESET);
+	ft_printf(COLOR_RED"%d %s\n"RESET, num, str);
+	ft_printf(COLOR_GREEN"The adress of Student is: "RESET);
+	ft_printf(COLOR_RED"%p\n"RESET, str);
+	ft_printf(COLOR_GREEN"The hexadecimal of 42 is: "RESET);
+	ft_printf(COLOR_RED"%x\n"RESET, num);
+	return (0);
+}
